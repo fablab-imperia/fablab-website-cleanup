d655f0b2eb8659f49ccbfba021c36682b3a364e6
Massimo Gismondi <omino.gis@gmail.com>
2024-02-01 22:18:21 +0100

    Aggiunto menu mobile e un quintale di pulizia css

--- index.php
+++ index.php
@@ -9,69 +9,82 @@ print_r($db->event_fetch_one(1));
 ?>
 
 <div style='background-image:url("/assets/images/hero-alto.opti.webp")' class="hero-container">
-    <div class="bootstrap-wrapper">
-        <div class="container">
-            <div class="hero">
-                <small>
-                    LABORATORIO DIGITALE CONDIVISO
-                </small>
-                <h1>Dai vita alle tue idee</h1>
-                <p>
-                Stampanti 3D, Fresatrice CNC, banco elettronica,
-                componenti e attrezzi meccanici e
-                tutto l'occorrente per creare (quasi) qualsiasi cosa... insieme
-                </p>
+    <div class="container">
+        <div class="hero">
+            <small class="text-highlight">
+                LABORATORIO DIGITALE CONDIVISO
+            </small>
+            <h1>Dai vita alle tue idee</h1>
+            <p>
+            Stampanti 3D, Fresatrice CNC, banco elettronica,
+            componenti e attrezzi meccanici e
+            tutto l'occorrente per creare (quasi) qualsiasi cosa... insieme
+            </p>
 
-                <div class="hero-buttons">
-                    <a href="" class="btn btn-primary">Iscriviti al Fablab!</a>
-                    <a  href="" class="btn">Scopri di più</a>
-                </div>
+            <div class="hero-buttons">
+                <a href="" class="btn btn-primary">Iscriviti al Fablab!</a>
+                <a  href="" class="btn">Scopri di più</a>
             </div>
-        <!-- <img class="hero-img" src="/assets/images/hero-alto.opti.webp" alt=""> -->
         </div>
+    <!-- <img class="hero-img" src="/assets/images/hero-alto.opti.webp" alt=""> -->
     </div>
-</div>    
+</div>
 
 
 
 
-<main>
-
-<div class="bootstrap-wrapper">
-<section>
-<div class="row">
-    <div class="col-md-6">
-        <small>IL LABORATORIO</small>
-        <h2>Sperimenta con la tecnologia</h2>
-        <p>
-        Tutti i servizi del Fablab possono essere utilizzati autonomamente dagli associati (previo corso).
-        </p>
-        <p>
-            Vorresti realizzare un oggetto con la Stampa 3D?
-            <br>
-            Hai già un'idea e vuoi realizzare un prototipo?
-        </p>
-
+<main class="container">
+    <section>
         <div class="row">
             <div class="col-md-6">
-                <img class="corner" src="/assets/images/IMG_5350 cropped-min.jpg" alt="">
-                <p>AAA</p>
+                <h2><small>IL LABORATORIO</small>Sperimenta con la tecnologia</h2>
+                <p>
+                Tutti i servizi del Fablab possono essere utilizzati autonomamente dagli associati (previo corso).
+                </p>
+                <p>
+                    Vorresti realizzare un oggetto con la <span class="text-highlight">Stampa 3D</span>?
+                    <br>
+                    Hai già un'idea e vuoi <span class="text-highlight">realizzare un prototipo</span>?
+                </p>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <img class="rounded wide" src="/assets/images/IMG_5350 cropped-min.jpg" alt="">
+                        <h3>Creatività 2.0</h3>
+                        <ul>
+                            <li>Stampa3D</li>
+                            <li>Intaglio vinile</li>
+                            <li>Fresatura CNC</li>
+                            <li>Elettronica</li>
+                            <li>Meccanica</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="rounded wide" src="/assets/images/ruoterover cropped-min.opti.png" alt="">
+                    </div>
+                </div>
+
             </div>
             <div class="col-md-6">
-                <img class="corner" src="/assets/images/ruoterover cropped-min.opti.png" alt="">
-                <p>AAA</p>
+                <img src="/assets/images/IMG_2557-min.jpg" alt="" class="rounded wide">
             </div>
         </div>
+    </section>
 
-    </div>
-    <div class="col-md-6">
-        <img src="/assets/images/IMG_2557-min.jpg" alt="" class="corner">
-    </div>
-</div>
-
-</section>
-</div>
-
+    <section>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="/assets/images/IMG_8103 figma-min.opti.webp" alt="" class="img-fluid rounded wide">
+            </div>
+            <div class="col-md-6">
+                <h2>
+                <small>I CORSI</small>
+                Se faccio, imparo!
+                </h2>
+                
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php
--- private/header.php
+++ private/header.php
@@ -8,19 +8,28 @@
     <link rel="stylesheet" href="/assets/bootstrap-grid.min.css">
     <link rel="stylesheet" href="/assets/font_load.css">
     <link rel="stylesheet" href="/assets/fabstyle.css">
+    <link rel="stylesheet" href="/assets/navbar.css">
 </head>
 
 <header>
     <nav>
-        <div class="navbar-title">
-            <img src="/assets/fab_icon.svg" alt="Icona rete fablab">
-            <span class="bold">FAB</span>LAB&nbsp;<span class="bold">IMPERIA</span>
-        </div>
+        <a href="/">
+            <div class="navbar-title">
+                <img src="/assets/fab_icon.svg" alt="Icona rete fablab">
+                <span class="bold">FAB</span>LAB&nbsp;<span class="bold">IMPERIA</span>
+            </div>
+        </a>
+        <input type="checkbox" id="nav_toggle_checkbox">
         <div class="navbar-menu">
             <a href="/">Home</a>
             <a href="/associazione/">Fablab</a>
             <a href="/eventi/">Eventi</a>
             <a href="/blog/">Blog</a>
         </div>
+        <label for="nav_toggle_checkbox">
+            <img src="/assets/images/hamburger.svg" id="hamburger-menu-icon" alt="">
+        </label>
     </nav>
 </header>
+
+<div class="bootstrap-wrapper">
